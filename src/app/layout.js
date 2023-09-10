f43ff24e444b7f366df5973826149774011031db
柿山幹雄 <kakicg@cgkojo.com>
2023-09-11 02:53:05 +0900

    create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from '@/components/ Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,13 @@ export default function RootLayout({ children }) {
       <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+        <Header />
+        {/* Navbar */}
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
   )
 }
