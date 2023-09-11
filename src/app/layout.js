cb753aecb476b8cfbb9e8753b42ee487cd53f09c
柿山幹雄 <kakicg@cgkojo.com>
2023-09-11 16:52:56 +0900

    create navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from '@/components/ Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Providers from "./Providers"
+import Navbar from '@/components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
           {/* Header */}
           <Header />
           {/* Navbar */}
+          <Navbar />
           {/* SearchBox */}
           {children}
         </Providers>
