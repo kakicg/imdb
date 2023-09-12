734c7b2fac94eafee4801472d3dca1d6ec7c8d34
柿山幹雄 <kakicg@cgkojo.com>
2023-09-12 22:47:37 +0900

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { HiOutlineThumbUp } from 'react-icons/hi'
+
+export default function Card({result}) {
+  return (
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
+        <Link href={`/movie/${result.id}`}>
+            <Image src={`https://image.tmdb.org/t/p/original${result.backdrop_path || result.poster_path}`} width={500} height={300} layout='responsive'
+            className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200' 
+            style={{
+                maxWidth: '100%',
+                height: 'auto',
+            }}
+            placeholder="blur" blurDataURL="/spinner.svg" alt="image is not available" />
+            <div className="p-2">
+                <p className='line-clamp-2 text-md'>{result.overview}</p>
+                <h2 className='truncate text-ls font-bold'>{result.title || result.name}</h2>
+               
+                <div className="flex items-center">
+                <p>
+                    {result.release_date || result.first_air_date}
+                </p>
+                <HiOutlineThumbUp className='h-5 ml-3 mr-1' /> {result.vote_count}
+                </div>
+            </div>
+        </Link>
+    </div>
+  )
+}
